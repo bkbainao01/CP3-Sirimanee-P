1c4646a4efb2c01a626dce0e2a1a689af19ce6f3
bkbainao01 <FERN.POBBY@GMAIL.COM>
2023-01-17 16:52:36 +0700

    Add exercise to repo
    Exercise4 from course  Complete Python Programing on academy.borntodev

---
+++ Exercise4_Sirimanee_P.py
@@ -0,0 +1,9 @@
+FE = 80.10
+GB = 69.20
+ICS = 78.30
+CP = 65.00
+print("---Your Score--- ")
+print("Foundation English : ", FE)
+print("General Business : ", GB)
+print("Introduction to Computer System : ", ICS)
+print("Computer Programing : ", CP)
